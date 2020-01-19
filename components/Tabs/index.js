2c6210bed1bbaf9baf8f068f2fcf6e8adaef72ba
Kolade-Junaid <koladejun@yahoo.com>
2020-01-19 18:59:10 +0000

    Modified the components/Tabs/index.js file

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,27 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    // console.log(response);
+    const newTopics = response.data.topics;
+    newTopics.forEach(topic => {
+      topicsElement.appendChild(tabCreator(topic));
+    });
+  });
+
+const topicsElement = document.querySelector(".topics");
+
+function tabCreator(object) {
+  const tabTopic = document.createElement("div");
+
+  tabTopic.classList.add("tab");
+
+  tabTopic.textContent = object;
+
+  // console.log(tabTopic);
+
+  return tabTopic;
+}
