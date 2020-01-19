feb3e3f328bc7acdb5b87e552c2c4f570fc86d69
Kolade-Junaid <koladejun@yahoo.com>
2020-01-19 19:09:49 +0000

    Modified the components/Cards/index.js

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,53 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    // console.log(response);
+    const articles = response.data.articles;
+    Object.keys(articles).forEach(category => {
+      articles[category].forEach(article =>
+        cardsContainer.appendChild(
+          cardCreator(article.headline, article.authorPhoto, article.authorName)
+        )
+      );
+    });
+  });
+
+  const cardsContainer = document.querySelector('.cards-container');
+
+  function cardCreator(articleHeadline, articleAuthorPhoto, articleAuthorName) {
+    const cardDiv = document.createElement("div");
+    const headline = document.createElement("div");
+    const author = document.createElement("div");
+    const imgContainer = document.createElement("div");
+    const authorImg = document.createElement("img");
+    const writtenBy = document.createElement("span");
+
+    cardDiv.classList.add("card");
+    headline.classList.add("headline");
+    author.classList.add("author");
+    imgContainer.classList.add("img-container");
+  
+    authorImg.src = articleAuthorPhoto;
+  
+    headline.textContent = articleHeadline;
+    writtenBy.textContent = `By: ${articleAuthorName}`;
+  
+    cardDiv.appendChild(headline);
+    cardDiv.appendChild(author);
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(authorImg);
+    author.appendChild(writtenBy);
+  
+    console.log(cardDiv);
+  
+    return cardDiv;
+  }
+  
+  
+
+
+
